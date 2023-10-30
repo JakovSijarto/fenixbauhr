77fe55c6ae216670f4cd8848a2197bcaa5d698f3
JakovSijarto <jakov.sijarto2@gmail.com>
2023-10-30 13:00:40 +0100

    vizitka

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    TEST
+  <div class="background-image w-[100vw] h-[100vh] bg-black">
+    <div class="w-full flex justify-center items-center h-full">
+      <img src="../../public/photos/vizitka.png" alt="" class="w-[60em] h-[40em] max-[1000px]:w-[35em] max-[1000px]:h-[20em] max-[570px]:h-[10em] max-[570px]:w-[20em] max-[340px]:h-[10em] max-[340px]:w-[16em]">
+    </div>
   </div>
 </template>
 
@@ -8,6 +10,12 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.background-image{
+  background: url("../../public/photos/bg-photo.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-size: cover;
+}
 
 </style>
