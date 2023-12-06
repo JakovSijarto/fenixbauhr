3ed1db90c4a33f6509679f8850906150614aa91e
JakovSijarto <jakov.sijarto2@gmail.com>
2023-12-06 19:16:20 +0100

    mjau

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,17 @@
       <img src="../../public/photos/vizitka.png" alt="" class="w-[60em] h-[40em] max-[1000px]:w-[35em] max-[1000px]:h-[20em] max-[570px]:h-[10em] max-[570px]:w-[20em] max-[340px]:h-[10em] max-[340px]:w-[16em]">
     </div>
   </div>
+  <div class="flex flex-wrap justify-center">
+      <img src="../assets/photos_working/photo1.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo2.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo3.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo4.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo5.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo6.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo7.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo8.webp" alt="" class="w-[600px] h-[600px] object-cover">
+      <img src="../assets/photos_working/photo9.webp" alt="" class="w-[600px] h-[600px] object-cover">
+  </div>
 </template>
 
 <script setup>
