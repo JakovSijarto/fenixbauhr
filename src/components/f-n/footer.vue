16d832ab6f92a119b65a7f907927261cb357f091
JakovSijarto <jakov.sijarto2@gmail.com>
2023-12-30 14:48:26 +0100

    a

--- src/components/f-n/footer.vue
+++ src/components/f-n/footer.vue
@@ -6,7 +6,6 @@
                 <span class="font-bold">Kontakt:</span>
                 <span><a href="mailto:info@fenixbauhr.com">info@fenixbauhr.com</a></span>
                 <span><a href="tel:+386953803940">(+385) 95-380-3940</a></span>
-                <span><a href="tel:+385976419336">(+385) 97-641-9336</a></span>
             </li>
             <li class="max-[360px]:hidden"><img src="/photos/newlogo-removedbg.png" alt="" class="w-[100px] "></li>
             <li class="flex flex-col gap-[0.2em]">
